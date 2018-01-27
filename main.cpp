0bb20793fe172095d9eb59c476bb54fab22e62f0
dunaevaa <32384871+dunaevaa@users.noreply.github.com>
2018-01-27 23:02:32 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<locale>
+#include<string>
 
 using namespace std;
 
@@ -16,7 +17,6 @@ void menu() {
 
 struct Node {
     int data;
-    Node *last;
     Node *next;
 };
 struct List {
@@ -24,15 +24,6 @@ struct List {
     Node *last;
 };
 
-void list_show(List list) {
-    while (list.head != nullptr) {
-        cout << list.head -> data;
-        if (list.head -> next != nullptr)
-            cout << "->";
-        list.head = list.head -> next;
-    }
-    cout << endl;
-}
 void create_list(List &list, int *&mass, int n) {
     for (int i=0; i<n; i++) {
         Node *node = new Node;
@@ -48,6 +39,38 @@ void create_list(List &list, int *&mass, int n) {
     }
 }
 
+void list_show(List list) {
+    while (list.head != nullptr) {
+        cout << list.head -> data;
+        if (list.head -> next != nullptr)
+            cout << "->";
+        list.head = list.head -> next;
+    }
+    cout << endl;
+}
+
+void add_elements(List &list) {
+    int k = 0;
+    cout << "Введите количество элементов" << endl;
+    cin >> k;
+    int *dop_mass = new int [k];
+    cout << "Введите элементы" << endl;
+    for (int i=0; i<k; i++) 
+        cin >> dop_mass[i];
+    for (int i=0; i<k; i++) {
+        Node *node = new Node;
+        node -> data = dop_mass[i];
+        node -> next = nullptr;
+        if (list.last == nullptr) {
+            list.last = node;
+            list.head = list.last;
+            continue;
+        }
+        list.last -> next = node;
+        list.last = node;
+    }    
+}
+
 int main(int argc, char *argv[]) {
     setlocale(LC_ALL, "Russian");
     List list;
@@ -60,14 +83,36 @@ int main(int argc, char *argv[]) {
     cout << "Введите элементы" << endl;
     for (int i=0; i<n; i++) 
         cin >> mass[i];
-        
+    int *dop_mass = nullptr;
+    int k;   
     create_list(list, mass, n);
     int choise;
-    while (true) {
+    bool vihod = true;
+    while (vihod) {
         menu();
         cin >> choise;
         switch (choise) {
             case 1: list_show(list);
+            break;
+            case 2: add_elements(list);
+            break;
+            case 3:
+            break;
+            case 4:
+            break;
+            case 5:
+            break;
+            case 6:
+            break;
+            case 7:  
+                cout << "Вы хотите выйти из программы?" << endl;
+                string answer;
+                cin >> answer;
+                if ((answer == "yes") || (answer == "y") || (answer == "Y") || (answer == "Yes") || (answer == "YES"))
+                    vihod = false;
+                else
+                if ((answer == "no") || (answer == "n") || (answer == "N") || (answer == "No") || (answer == "NO"))
+                    vihod == true;
             break;
             return 0;
             default: cout << "Неверная команда" << endl;
